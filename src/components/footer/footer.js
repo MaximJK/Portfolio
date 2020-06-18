6f1a3c3006a20b01d92007c52e9a0f80d047091e
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-06-18 19:28:10 -0400

    draft 2

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,19 +1,12 @@
 import React from "react"
 import footerStyles from "./footer.module.css"
 export default function Footer(props) {
-  function handleBodyClick(comp) {
-    props.setBody(comp);
-  }
-  let backButton = ''
-  if (props.content !== 'links') {
-    backButton = <p onClick={() => handleBodyClick('links')}>back</p>
 
-  }
   return (
      <div className={footerStyles.footer}>
         <div>
-        {backButton}
-  
+        
+        © Max Koiwai ~ built with Gatsby
     </div>
   </div>
   )
