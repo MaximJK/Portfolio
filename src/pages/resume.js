548f4725e5f8f1ab3d4af64be8310d86c005c175
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-06-18 19:31:59 -0400

    build error

--- src/pages/resume.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import resumepdf from "../../static/headshot.png"
-// export default function Resume() {
-
-//     return (
-//         <img src={resumepdf}></img>
-//     )
-// }
